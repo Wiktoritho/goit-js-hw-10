64553f4df4fd84da35629cce38662c0878ca19b6
Wiktor Gola <wiktor.gola.95@wp.pl>
2023-01-12 21:22:01 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 let _ = require('lodash');
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
@@ -8,10 +9,8 @@ const inputCountry = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-const fetchCountries = name => {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  )
+const fetching = name => {
+  return fetchCountries(name)
     .then(response => {
       if (!response.ok) {
         throw new Error(response.status);
@@ -19,26 +18,22 @@ const fetchCountries = name => {
       return response.json();
     })
     .then(data => {
-      console.log(data.length);
       if (data.length > 10) {
         Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
       } else if (data.length <= 10 && data.length > 1) {
         data.forEach(country => {
-          // Lists up to 10 countries that match entered value
           countryListEl(country);
         });
       } else {
         data.forEach(country => {
-          // Creates html element for country with more detailed information
           countryInfoDetails(country);
         });
       }
     })
     .catch(() => Notify.failure('Oops, there is no country with that name'));
 };
-
 
 inputCountry.addEventListener(
   'input',
@@ -48,8 +43,7 @@ inputCountry.addEventListener(
     if (inputCountry.value == '') {
       return;
     } else {
-      fetchCountries(inputCountry.value.trim());
-
+      fetching(inputCountry.value.trim());
     }
   }, DEBOUNCE_DELAY)
 );
