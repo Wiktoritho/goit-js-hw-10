46ee836025ade017af86b15a61451f62affe5a1d
Wiktor Gola <wiktor.gola.95@wp.pl>
2023-01-12 20:49:23 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 let _ = require('lodash');
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
 const inputCountry = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
+
+const fetchCountries = name => {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data.length);
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length <= 10 && data.length > 1) {
+        data.forEach(country => {
+          // Lists up to 10 countries that match entered value
+          countryListEl(country);
+        });
+      } else {
+        data.forEach(country => {
+          // Creates html element for country with more detailed information
+          countryInfoDetails(country);
+        });
+      }
+    })
+    .catch(() => Notify.failure('Oops, there is no country with that name'));
+};
+
 
 inputCountry.addEventListener(
   'input',
